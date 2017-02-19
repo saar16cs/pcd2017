ba19febec29f70dcace4d3b630bbc10aff63f933
Your Name <you@example.com>
2017-02-19 21:22:33 +0530

    roots of quadratic equation

---
+++ week1/rootsofquadratic.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+float a,b,c,d,e;
+printf("enter the quadratic equation as ax2+bx+c where a,b,c are constants");
+scanf("%fx2+%fx+%f", &a,&b,&c);
+if(a*b*c==0)
+{printf("invalid quadratic equation");}
+else
+{
+d=(b*b)-(4*a*c);
+e=sqrt(d);
+if(e>=0)
+{
+printf("real roots are %f and %f\n", (-b+e)/(2*a), (-b-e)/(2*a));
+}
+else
+{printf("roots are imaginary");}
+}
+return 0;
+}
