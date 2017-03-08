a1b44f664d0fa6317b6546f7e59022bd6b7ec19e
shashi kumar <saar16cs@cmrit.ac.in>
2017-03-08 15:35:33 +0530

    Add files via upload

--- week2/polynomial.c
+++ week2/polynomial.c
@@ -1,34 +1,36 @@
 #include<stdio.h>
-int polnomial(int a[], int x)
+int polynomial(int a[], int x, int n)
 	{
 	int sum=0,i;
-	for(i=n;i>0;i--)
+	for(i=n-1;i>0;i--)
 		{
 		sum=(sum+a[i])*x;
 		}	
 	return sum+a[0];
 	}
 
-int get_polynomial(int n)
+int get_polynomial()
 {
 	int a;
-	pritnf("enter the coefficients");
-	scanf("%d", a);
+	
+	scanf("%d", &a);
+	return a;
 }
 
 int main()
 {
-int n,x;
+	int n,x,sum,i;
 	printf("enter the order of polynomial");
 	scanf("%d", &n);
-int a[n+1];
+	int a[n+1];
+	printf("enter the coefficients");
 	for(i=n;i>=0;i--)
 	{
-		a[i]=get_polynomial(n);
+		a[i]=get_polynomial();
 	}
 	printf("enter x");
 	scanf("%d", &x);
-	sum=polynomial(a,x);
+	sum=polynomial(a,x,n+1);
 	printf("%d",sum);
 	return 0;
 }
